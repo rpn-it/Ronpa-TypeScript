071b0c1d950e9688e847ebac2efdbec3f169277a
WMXPY <wm@mengw.io>
2019-10-27 15:43:45 -0500

    init story

---
+++ src/story.ts
@@ -0,0 +1,17 @@
+/**
+ * @author WMXPY
+ * @namespace Ronpa
+ * @description Story
+ */
+
+export class Story {
+
+    public static create() {
+
+        return new Story();
+    }
+
+    private constructor() {
+
+    }
+}
