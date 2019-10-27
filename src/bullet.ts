7415f760c17addbdc7b324972771014d3e0b4ca4
WMXPY <wm@mengw.io>
2019-10-27 15:45:29 -0500

    init bullet

---
+++ src/bullet.ts
@@ -0,0 +1,17 @@
+/**
+ * @author WMXPY
+ * @namespace Ronpa
+ * @description Bullet
+ */
+
+export class Bullet {
+
+    public static create() {
+
+        return new Bullet();
+    }
+
+    private constructor() {
+
+    }
+}
