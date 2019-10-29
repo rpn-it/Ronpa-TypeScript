5ce524befb80046486b6ec6461ae5f816af21325
WMXPY <wm@mengw.io>
2019-10-29 11:16:39 -0500

    implement more tests

--- test/unit/ronpa.test.ts
+++ test/unit/ronpa.test.ts
@@ -29,4 +29,17 @@ describe('Given {Ronpa} Class', (): void => {
         expect(ronpa.flat()).to.be.deep.equal(records);
         expect(ronpa).to.be.lengthOf(records.length);
     });
+
+    it('should be able to create story', (): void => {
+
+        const by: string = chance.string();
+        const content: string = chance.string();
+
+        const ronpa = Ronpa.create();
+
+        ronpa.createStory(by, content);
+
+        expect(ronpa.flat()).to.be.lengthOf(1);
+        expect((ronpa.flat()[0] as any).thesis.insiders).to.be.lengthOf(0);
+    });
 });
