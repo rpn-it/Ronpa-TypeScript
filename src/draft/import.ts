a1fb0ae784d1fdd245fde9738f3f6e667887341b
WMXPY <wm@mengw.io>
2019-11-20 11:00:27 -0600

    use new actions

--- src/draft/import.ts
+++ src/draft/import.ts
@@ -5,12 +5,13 @@
  */
 
 import { RECORD_TYPE, RONPA_ACTION } from "../declare";
-import { ReactionChange } from "./reaction";
+import { AddReactionChange, RemoveReactionChange } from "./reaction";
 import { ThesisChange } from "./thesis";
 
 export type ChangeType<C extends RONPA_ACTION, T extends RECORD_TYPE = RECORD_TYPE.TEXT> =
     C extends RONPA_ACTION.ADD_THESIS ? ThesisChange<T>
-    : C extends RONPA_ACTION.REACTION ? ReactionChange
+    : C extends RONPA_ACTION.ADD_REACTION ? AddReactionChange
+    : C extends RONPA_ACTION.REMOVE_REACTION ? RemoveReactionChange
     : never;
 
 export * from "./reaction";
